d985d20f85c59b20e913b5847f04bbbd53c2e3af
mooeypoo <moriel@gmail.com>
2025-04-15 16:41:23 +0300

    Try github pages vite config with repo base

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    // base: '/FuncyStr/',
-    base: './', // Try using relative paths for Github Pages
+    base: '/FuncyStr/',  // Use the repository name as the base path
     root: 'public',
     build: {
         outDir: '../dist',
         emptyOutDir: true
     }
-}); 
+});
